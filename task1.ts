6eb10e4a149fb434d0bd4aabb0f583725996ee3c
Uladzimir <humanamburu@gmail.com>
2022-04-28 12:10:55 +0200

    task1

---
+++ task1.ts
@@ -0,0 +1,30 @@
+
+// Implement a function that generates a stats object for the movies array in the format
+// { [genre]: { averageRating: number, titles: string[] }}
+const getMoviesStatsByGenre = (movies, startFromYear) => {
+    // implementation
+};
+
+// Example:
+
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    director: "Frank Darabont",
+    duration: "2h 22min",
+    genre: ["Crime", "Drama"],
+    rate: 9.3,
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    duration: "2h 55min",
+    genre: ["Comedy"],
+    rate: 9.2,
+  }
+]
+
+getMoviesStats(movies, 1972);
+// { crime: { averageRate: 8, titles: ['The Shawshank Redemption'] }, drama: {...} }
